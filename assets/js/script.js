d885dfbd47ebce6aa78592a6217e7e6cf2f23a02
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-14 15:35:57 +0200

    add markup in DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,63 +3,77 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
 
 
+// seleziono l'elemento dalla DOM
+const memberCardsEL = document.getElementById("member-cards");
+// console.log(memberCardsEL);
 
+// ciclo all'interno dell'array per recuperare il singolo membro
+for (let i = 0; i < teamMembers.length; i++) {
 
+  // salvo il singolo membro all'interno di una costante
+  const singleMember = teamMembers[i];
 
+  // invoco la funzione per recuperare le proprietà del singolo oggetto
+  const markup = getMemberCardMarkup(singleMember);
+  console.log(markup);
+
+  // aggiungo il markup alla DOM
+  memberCardsEL.insertAdjacentHTML("beforeend", markup);
+};
 
 
 
 function getMemberCardMarkup(member) {
 
-  const {name, role, email, image} = member;
+  const {name, role, email, img} = member;
 
 
   return`
-    <div class="col-12 col-md-6 mb-4">
+    <div class="col-12 col-md-6 col-xl-4 mb-4">
       <div class="d-flex align-items-center bg-dark">
-          <div class="col-3">
-              <img src="${image}" alt="">
-          </div>
-          <div class="col-9 ps-3 text-light">
-              <h5 class="fw-bold">${name}</h5>
-              <div>${role}</div>
-              <a href="#" class="e-mail">${email}</a>
-          </div>
+        <div class="col-3">
+          <img src="${img}" alt="" class="">
+        </div>
+        <div class="col-9 ps-3 text-light">
+          <h5 class="fw-bold">${name}</h5>
+          <div>${role}</div>
+          <a href="#" class="e-mail">${email}</a>
+        </div>
       </div>
     </div>
   `;
